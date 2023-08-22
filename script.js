61b0f9284414a0a29cc34821c073b66812aae892
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-08-22 20:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.getElementsByClassName('price');
+   const pricesArr=Array.from(prices);
+	let sum=0;
+	pricesArr.forEach((item) =>{
+		sum=sum+Number(item.innerText);
+	});
+	const table=document.getElementsByTagName("table")[0];
+	const tr=document.createElement("tr");
+	const td=document.createElement("td");
+	td.innerText=`Total price is ${sum}`;
+	td.colspan="2";
+	td.id="ans";
+	tr.append(td);
+	table.append(tr);
+	
   
 };
 
 getSumBtn.addEventListener("click", getSum);
-
